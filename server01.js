4f9c58fd7799485821e04bde50335eb87ca820ef
Joshua Lloyd <joshualloyd@joshualloyd.com>
2017-09-07 10:51:33 -0500

    refactored .listen and added clickable link in console

--- server01.js
+++ server01.js
@@ -3,8 +3,6 @@ const fs = require('fs');
 
 const server = http.createServer((request, response) => {
   fs.createReadStream('index.html').pipe(response);
-});
-
-server.listen(8080, ()=>{
-  console.log('listening on PORT 8080');
+}).listen(8080, ()=>{
+  console.log('listening on http://localhost:8080');
 });
